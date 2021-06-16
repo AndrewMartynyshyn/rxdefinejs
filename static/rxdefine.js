6641c8af5a9ebccdd341d7a5c96bb64bbb36495d
Andrew Martynyshyn <andrew.martynyshyn@rxdefine.com>
2021-06-16 15:10:25 +0300

    updated file

--- static/rxdefine.js
+++ static/rxdefine.js
@@ -1,8 +1,6 @@
 alert("hello world");
 
-///WWW.BOUCH.COM
-
-///Infinity.RXDEFINE.APP
-
 document.cookie =
   "username=John Doe; expires=Thu, 18 Dec 2013 12:00:00 UTC; path=/";
+
+alert(document.cookie);
