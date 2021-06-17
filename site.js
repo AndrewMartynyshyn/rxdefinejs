cd7e9bfd1d8af8d2add4fbae48b88cffd7a4ce91
Andrew Martynyshyn <andrew.martynyshyn@rxdefine.com>
2021-06-17 16:29:54 +0300

    added cors; updated rxdefine.js

--- site.js
+++ site.js
@@ -4,6 +4,15 @@ var cookieParser = require("cookie-parser");
 
 app.use(cookieParser());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("*", function (req, res, next) {
   var rxid = req.cookies.rxid;
 
