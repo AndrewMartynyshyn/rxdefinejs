627207830e803aade13089e31f47c2b9f12875df
Andrew Martynyshyn <andrew.martynyshyn@rxdefine.com>
2021-06-24 13:36:04 +0300

    added rewrite utm params function

--- static/rxdefine.js
+++ static/rxdefine.js
@@ -31,52 +31,48 @@ function grabFormData() {
   xhr.send(JSON.stringify(result));
 }
 
-function grabUtms() {
+function rewriteUrls() {
   const links = [...document.getElementsByTagName("A")].filter((el) =>
     el.href.includes("rxengage")
   );
 
   let params = {};
   const posthogProps = window.posthog.persistence.props;
+
+  if (posthogProps.distinct_id !== undefined) {
+    params += "&ph_id=" + posthogProps.distinct_id;
+  }
+
   if (posthogProps.utm_campaign !== undefined) {
     params["utm_campaign"] = posthogProps.utm_campaign;
   }
+
   if (posthogProps.utm_medium !== undefined) {
     params["utm_medium"] = posthogProps.utm_medium;
   }
+
   if (posthogProps.utm_source !== undefined) {
     params["utm_source"] = posthogProps.utm_source;
   }
+
   if (posthogProps.utm_content !== undefined) {
     params["utm_content"] = posthogProps.utm_content;
   }
 
-  var xhr = new XMLHttpRequest();
-  xhr.withCredentials = true;
-  xhr.open("POST", "https://rxdefinejs.herokuapp.com/utms", true);
+  document.getElementById("params").innerHTML = params;
 
-  xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-
-  xhr.onreadystatechange = function (res) {
-    if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-      const res = JSON.parse(xhr.response);
-      const container = $("#result");
-
-      for (var key in res) {
-        container.append("<div>");
-        container.append("<span>" + key + ": </span>");
-        container.append("<strong>" + res[key] + "</strong>");
-        container.append("</div>");
-      }
+  links.forEach((link) => {
+    if (link.href.includes("?")) {
+      link.setAttribute("href", link.href + params);
+    } else {
+      link.setAttribute("href", `${link.href}?${params.slice(1)}`);
     }
-  };
-
-  xhr.send(JSON.stringify(params));
+  });
 }
 
 (function waitForPosthog() {
   if (window.posthog && window.posthog.__loaded) {
-    grabUtms();
+    rewriteUrls();
   } else {
     setTimeout(waitForPosthog, 10);
   }
